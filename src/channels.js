4021aca1028b0dd0899d7f18c96bb0b466683a41
Zirui Li <z5423101@ad.unsw.edu.au>
2023-02-20 21:51:02 +1100

    Stub for channelsListV1 function

---
+++ src/channels.js
@@ -0,0 +1,12 @@
+// Sample stub for the channelsListV1 function
+// Return stub value matches table below
+function channelsListV1(email, password) {
+  return {
+    channels: [
+      {
+        channelId: 1,
+        name: 'My Channel',
+      }
+    ],
+  }
+}
