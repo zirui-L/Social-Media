42ce1e67935f989bdd70f718f1274762317419c0
“Esme <z53554333@ad.unsw.edu.au>
2023-03-04 23:50:52 +1100

    channelInviteV1 re-merge

--- src/channel.js
+++ src/channel.js
@@ -27,8 +27,46 @@ function channelJoinV1(authUserId, channelId) {
 }
 
 function channelInviteV1(authUserId, channelId, uId) {
-return {}
+  //input check 
+  let auth_check = 0;
+  let channel_check = 0;
+  let uid_check = 0;
+  let member_check = 1;
+  let temp = 0;
+  for (const i of data.users) {
+    if (i.uId === authUserId) {
+      auth_check = 1;
+    }
+    if (i.uId === uId) {
+      uid_check = 1;
+      temp = i;
+    }
+  }
+  for (const i of data.channels) {
+    if (i.channelId === channelId) {
+      channel_check = 1;
+      for (const k of i.allMembers) {
+        if (k.uId === uId) {
+          member_check = 0;
+        }
+      }
+    }
+  }
+  if (channel_check * auth_check * uid_check * member_check === 0) {
+    return {error: 'error'};
+  }
+  
+  // add member 
+  for (const i of data.channels) {
+    if (i.channelId === channelId) {
+      i.allMembers.push(temp);
+      return {};
+    }
+  }
+  return {};
 }
+
+
 
 function channelMessagesV1(authUserId, channelId, start) {
     //input check 
