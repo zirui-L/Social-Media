e8f2db127bc3bcba7aaa15759171f2699815a451
Esme Gao <esme.gao@student.unsw.edu.au>
2023-03-04 23:53:58 +1100

    Channel details + joins

--- src/channel.js
+++ src/channel.js
@@ -1,30 +1,114 @@
+
+
+
+
+const ERROR = {error: 'error'};
+
+/**
+  * Given a channel with ID channelId that the authorised user 
+  * is a member of, provides basic details about the channel.
+  * 
+  * @param {integer} authUserId - description of paramter
+  * @param {integer} channelId - description of parameter
+  * ...
+  * @returns {{name, isPublic, ownerMembers, allMembers}} - object
+  * @returns {{error: 'error'}} - when channelId/authUserId 
+  * is invalid or authorised user is not a member of the channel
+*/
+
 function channelDetailsV1(authUserId, channelId) {
-  return {
-    name: "Hayden",
-    ownerMembers: [
-      {
-        uId: 1,
-        email: "example@gmail.com",
-        nameFirst: "Hayden",
-        nameLast: "Jacobs",
-        handleStr: "haydenjacobs",
-      },
-    ],
-    allMembers: [
-      {
-        uId: 1,
-        email: "example@gmail.com",
-        nameFirst: "Hayden",
-        nameLast: "Jacobs",
-        handleStr: "haydenjacobs",
-      },
-    ],
-  };
+  let channelIdisValid = false;
+  let UserisMember = false;
+  let authUserIdisValid = false;
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId) {
+      channelIdisValid = true;
+      if (channel.allMembers.includes(authUserId)) {
+        UserisMember = true;
+      }
+    }
+    break;
+  }
+  for (const user of data.users) {
+    if (user.uId === authUserId) {
+      authUserIdisValid = true;
+    }
+    break;
+  }
+  if (!channelIdisValid || !UserisMember || !authUserIdisValid) {
+    return ERROR;
+  }
+
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId) {
+      let channelDetail = {
+        name: channel.name,
+        isPublic: channel.isPublic,
+        ownerMembers: [],
+        allMembers: [],
+      }
+      /* for (const owners of channel.ownerMembers) {
+        channelDetail.ownerMembers.push(userProfileV1(authUserId,uId));
+      } */
+
+      return channelDetail;
+    }
+  }
 }
 
+
+/**
+  * Given a channelId of a channel that the authorised user 
+  * can join, adds them to that channel.
+  * 
+  * @param {integer} authUserId - description of paramter
+  * @param {integer} channelId - description of parameter
+  * ...
+  * @returns {{}} - return empty object
+  * @returns {{error: 'error'}} - when channelId/authUserId 
+  * is invalid or authorised user is not a member of the channel
+  * or the channel is private and when the authorised user is 
+  * not a channel member and is not a global owner
+*/
+
 function channelJoinV1(authUserId, channelId) {
+  let channelIdisValid = false;
+  let UserisMember = false;
+  let authUserIdisValid = false;
+  let UserisGlobalOwner = false;
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId) {
+      channelIdisValid = true;
+      if (channel.allMembers.includes(authUserId)) {
+        UserisMember = true;
+      }
+    }
+    break;
+  }
+  for (const user of data.users) {
+    if (user.authUserId === authUserId) {
+      authUserIdisValid = true;
+    }
+    if (user.permissionId === 1) {
+      UserisGlobalOwner = true;
+    }
+    break;
+  }
+  if (!channelIdisValid || UserisMember || !authUserIdisValid ||
+      !UserisGlobalOwner) {
+    return ERROR;
+  }
+
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId) {
+      channel.allMembers.push(authUserId);
+    }
+    break;
+  }
   return {};
 }
+
+
 
 function channelInviteV1(authUserId, channelId, uId) {
   //input check 
