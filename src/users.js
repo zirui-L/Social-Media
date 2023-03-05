d3687de1d132a63549acbf698c9592df6e2608de
Zhenbo Zhang <zb.zhang.general@gmail.com>
2023-03-05 12:53:39 +1100

    finished implemented and test functions in users.js and other.js

--- src/users.js
+++ src/users.js
@@ -8,14 +8,14 @@ const ERROR = { error: "error" };
  * @param {string} integer - email address of the registered users
  * @param {string} integer - password of the registered users
  *
- * @returns {{ authUserId }} - valid email, password with more than 6 characters, and the length of name is between 1 and 50 inclusive
- *  @returns {{ error: "error" }} - invalid or alreade registered email, length of passwor is lower than 6, length of names is outside of the range [1,50]
+ * @returns {{ authUserId }} - return if the autherUserId and uId are both valid
+ *  @returns {{ error: "error" }} - return if there is invalid autherUserId or invalid uId
  */
 
 export function userProfileV1(authUserId, uId) {
   const data = getData();
 
-  if (!isUserValid(data, authUserId) || !isUserValid(data, uId)) return ERROR;
+  if (!isUserValid(data, authUserId) || !isUserValid(data, uId)) return ERR;
 
   for (const user of data.users) {
     if (user.authUserId === uId) {
